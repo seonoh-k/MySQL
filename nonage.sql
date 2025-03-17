f5055428fb62b82311259ee2e77d9ad2c001352d
seonoh.kim <kimseonoh101@gmail.com>
2025-03-17 21:09:20 +0900

    ohoho

--- nonage.sql
+++ nonage.sql
@@ -7,4 +7,15 @@ select * from product;
 select * from qna;
 select * from worker;
 
-select * from member where id = ks123 and pwd = 123123
+select * from member where id = ks123 and pwd = 123123;
+
+select * from order_view order by odseq desc;
+
+select * from order_view where id = 'ks123' and oseq = 22 and result = 1;
+
+select * from qna where qseq = 21;
+
+select  * 
+from ( select rownum rm,p1.* 
+from (select * from product where name like '%'||'힐'||'%' order by indate desc) p1)p2 
+where rm >= 1 and rm <= 5;
